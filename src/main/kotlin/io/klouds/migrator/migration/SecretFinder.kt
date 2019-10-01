a045c6605d645d676e8366938dd08f928f36b5f3
Chris Barbour <chris.barbour03@gmail.com>
2019-10-01 17:30:00 +0100

    fixing password retrieval

--- src/main/kotlin/io/klouds/migrator/migration/SecretFinder.kt
+++ src/main/kotlin/io/klouds/migrator/migration/SecretFinder.kt
@@ -4,6 +4,8 @@ import com.amazonaws.regions.Regions
 import com.amazonaws.services.secretsmanager.AWSSecretsManager
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClient
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.klouds.migrator.Handler.Companion.objectMapper
 
 interface SecretFinder {
     fun secretFor(key: String): String
@@ -13,5 +15,7 @@ class AwsSecretFinder(
     private val secretsManager: AWSSecretsManager = AWSSecretsManagerClient.builder().withRegion(Regions.EU_WEST_1).build()
 ) : SecretFinder {
     override fun secretFor(key: String) =
-        secretsManager.getSecretValue(GetSecretValueRequest().withSecretId(key)).secretString
+        objectMapper.readValue<Secret>(secretsManager.getSecretValue(GetSecretValueRequest().withSecretId(key)).secretString).password
 }
+
+data class Secret(val password: String)
